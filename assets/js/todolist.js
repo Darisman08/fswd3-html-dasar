70be3662f400af702a40e4140e9c2deb6c8eea47
Darisman08 <darisman08@gmail.com>
2023-03-21 11:44:36 +0700

    [FEAT] Tugas 9

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -1,29 +1,35 @@
-// mendefinisikan variabel global
-let todoList = [];
+const baseUrl = "https://crudcrud.com/api/";
+const apiKey = "2dfca93e680e4aa9841d8da322243239";
+const url = baseUrl + apiKey + "/todoList";
 
-// mendapatkan data todoList dari local storage
-if(localStorage.getItem('todoList')){
-  todoList = JSON.parse(localStorage.getItem('todoList'));
+// mendapatkan data todoList dari REST API
+function getTodoList() {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      todoList = data;
+      isiTodoList();
+    });
 }
 
 // mengisi data todo list ke dalam html
 function isiTodoList() {
   const listElement = document.getElementById("todoList");
   listElement.innerHTML = "";
-  for(let i=0; i<todoList.length; i++){
+  for (let i = 0; i < todoList.length; i++) {
     const item = todoList[i];
     const todoElement = document.createElement("li");
     todoElement.innerHTML = item.todoText;
-    if(item.done){
+    if (item.done) {
       todoElement.style.textDecoration = "line-through";
     }
     const deleteButton = document.createElement("button");
     deleteButton.innerHTML = "x";
-    deleteButton.addEventListener("click", function(){
+    deleteButton.addEventListener("click", function () {
       hapusTodoList(i);
     });
     todoElement.appendChild(deleteButton);
-    todoElement.addEventListener("click", function(){
+    todoElement.addEventListener("click", function () {
       toggleTodoList(i);
     });
     listElement.appendChild(todoElement);
@@ -31,29 +37,55 @@ function isiTodoList() {
 }
 
 // menambahkan todo list
-function tambahTodo(){
+function tambahTodo() {
   const todoInput = document.getElementById("todoInput");
   const todoText = todoInput.value;
-  if(todoText){
-    todoList.push({todoText: todoText, done: false});
+  if (todoText) {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ todoText: todoText, done: false })
+    })
+      .then(response => response.json())
+      .then(data => {
+        todoList.push(data);
+        isiTodoList();
+      });
     todoInput.value = "";
-    isiTodoList();
-    localStorage.setItem('todoList', JSON.stringify(todoList));
   }
 }
 
 // menghapus todo list
-function hapusTodoList(index){
-  todoList.splice(index, 1);
-  isiTodoList();
-  localStorage.setItem('todoList', JSON.stringify(todoList));
+function hapusTodoList(index) {
+  const id = todoList[index]._id;
+  fetch(url + "/" + id, {
+    method: "DELETE"
+  })
+    .then(response => {
+      todoList.splice(index, 1);
+      isiTodoList();
+    });
 }
 
 // menandai todo list selesai
-function toggleTodoList(index){
-  todoList[index].done = !todoList[index].done;
-  isiTodoList();
-  localStorage.setItem('todoList', JSON.stringify(todoList));
+function toggleTodoList(index) {
+  const item = todoList[index];
+  const id = item._id;
+  const todoText = item.todoText;
+  const done = !item.done;
+  fetch(url + "/" + id, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ todoText: todoText, done: done })
+  })
+    .then(response => {
+      todoList[index].done = done;
+      isiTodoList();
+    });
 }
 
-isiTodoList(); // panggil fungsi isiTodoList() untuk mengisi data todo list ke dalam html saat halaman pertama kali dibuka
+getTodoList(); // panggil fungsi getTodoList() untuk mendapatkan data todo list dari REST API saat halaman pertama kali dibuka
